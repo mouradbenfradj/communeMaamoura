61ac384a8ee98874c1282192069d0aef2b6d2871
Mourad <mourad.ben.fradj@gmail.com>
2021-01-18 19:59:21 +0100

    knp_menu

---
+++ src/Menu/MenuBuilder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Menu;
+
+use Knp\Menu\FactoryInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
+
+class MenuBuilder
+{
+    private $factory;
+
+    public function __construct(FactoryInterface $factory)
+    {
+        $this->factory = $factory;
+    }
+
+    public function createMainMenu(RequestStack $requestStack)
+    {
+        $menu = $this->factory->createItem('root');
+
+        $menu->addChild('Home', ['route' => 'homepage']);
+        // ... add more children
+
+        return $menu;
+    }
+}
